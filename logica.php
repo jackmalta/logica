90c0cb7b137e413eeb1d02f8e4fffb4ccbf07e2c
php 09 <php.09@lampadinha.cec.ime.usp.br>
2018-01-19 20:23:28 -0200

    programa pares e impares

--- logica.php
+++ logica.php
@@ -2,16 +2,30 @@
 
   //Camel Case
 
-  //Repetir o nome
 
+  /* Repetir o nome
   for ($i = 0; $i < 100; $i++) {
     echo $i + 1 ." Isaque \n <br>";
   }
+  */
 
-  //Contar até 100
+  /*Contar até 100
 
   for ($i = 0; $i < 100; $i++) {
     echo $i+1 . "\n <br>";
   }
+
+  */
+
+  //Do 1 ao 100 diferenciando que curiosamente usa bool para o resultado verdadeiro (porque o resultado é 0, logo ele vira falso) e falso
+
+  for ($i = 1; $i < 101; $i++) {
+    if ($i % 2) {
+      echo "$i * \n <br>";
+    } else {
+      echo "$i ** \n <br> ";
+    }
+  }
+
 
 ?>
