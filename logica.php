0ee469142759a80d3917eac1e1b3879546eb899d
php 09 <php.09@lampadinha.cec.ime.usp.br>
2018-01-19 20:17:08 -0200

    Terceiro commit

--- logica.php
+++ logica.php
@@ -2,9 +2,16 @@
 
   //Camel Case
 
+  //Repetir o nome
+
   for ($i = 0; $i < 100; $i++) {
     echo $i + 1 ." Isaque \n <br>";
   }
 
+  //Contar até 100
+
+  for ($i = 0; $i < 100; $i++) {
+    echo $i+1 . "\n <br>";
+  }
 
 ?>
