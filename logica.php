e55a9811847e2bb5d9fec6158359c68612f39c76
php 09 <php.09@lampadinha.cec.ime.usp.br>
2018-01-19 20:31:17 -0200

    Programa PIN

--- logica.php
+++ logica.php
@@ -17,7 +17,7 @@
 
   */
 
-  //Do 1 ao 100 diferenciando que curiosamente usa bool para o resultado verdadeiro (porque o resultado é 0, logo ele vira falso) e falso
+  /*Do 1 ao 100 diferenciando que curiosamente usa bool para o resultado verdadeiro (porque o resultado é 0, logo ele vira falso) e falso
 
   for ($i = 1; $i < 101; $i++) {
     if ($i % 2) {
@@ -27,5 +27,30 @@
     }
   }
 
+  */
+
+  /*1, 2, 3, PIN!  Este jeito funcionou, mas não é legal
+  $count = 1;
+  for ($i = 1; $i <= 100; $i++) {
+    if ($count !== 4) {
+      echo "$i \n <br>";
+    } else {
+      echo "PIN \n <br>";
+      $count = 0;
+    }
+    $count++;
+  }
+
+  */
+
+  //Tentando de novo o do PIN
+
+  for ($i = 1; $i <= 100; $i++) {
+    if ($i % 4) {
+      echo "$i \n <br>";
+    } else {
+      echo "PIN \n <br>";
+    }
+  }
 
 ?>
